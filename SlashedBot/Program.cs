b7749265650dd53b600da09b3f2af8568f8d5384
Jorge González <iksaku@me.com>
2017-07-13 19:39:24 -0500

    Initial Commit

---
+++ SlashedBot/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus;
+
+namespace SlashedBot
+{
+	internal class Program
+	{
+		public static void Main(string[] args) {}
+
+		public static async Task Run()
+		{
+			var bot = new DiscordClient(new DiscordConfig
+			{
+				AutoReconnect = true,
+				DiscordBranch = Branch.Stable,
+				LargeThreshold = 250,
+				LogLevel = LogLevel.Debug,
+				Token = "",
+				TokenType = TokenType.Bot,
+				UseInternalLogHandler = true
+			});
+
+			await bot.ConnectAsync();
+			await Task.Delay(-1);
+		}
+	}
+}
