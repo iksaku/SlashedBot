edc64770981a0b04d415fe87c6b985e553232f9d
Jorge González <iksaku@me.com>
2017-07-17 17:04:12 -0500

    Fixed command execution, added some interesting .NET configurations

--- LibCommands/CommandManager.cs
+++ LibCommands/CommandManager.cs
@@ -26,8 +26,9 @@ namespace LibCommands
 
             string[] parts = ev.Message.Content.Split(' ');
             if (!Commands.TryGetValue(parts.First().Substring(2), out var command)) return;
-            string[] args = parts.Skip(0).ToArray();
-
+            string[] args = parts.Skip(1).ToArray();
+            Bot.DebugLogger.LogMessage(LogLevel.Debug, "CommandManager", $"Executing command '{command.Name}' with arguments: [{string.Join(", ", args)}]", DateTime.Now);
+            
             command.Execute(ev, args);
         }
 
